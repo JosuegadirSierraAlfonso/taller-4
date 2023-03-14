f18f8da0c8ea4edeb72e083800558a25ffa3619b
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-14 08:43:57 -0500

    feat: :tada: menu

---
+++ main.js
@@ -0,0 +1,17 @@
+alert(`-----MENU-----\n
+1.   Data reading\n
+2.   Create object\n
+3.   Show object\n
+4.   Create array\n
+5.   Show array\n
+6.   Remove the first element from array\n
+7.   Remove last element from array\n
+8.   Remove any element from array\n
+9.   Add element to beginning of array\n
+10.  Add element to end of array\n
+11.  Create array with objects\n
+12.  Iterate array with objects with FOR\n
+13.  Iterate array with objects with ForEach\n
+14.  Iterate array with objects with Map and create copy personal process\n
+15.  Personal process\n
+0.   Exit`)
